e28d8fb0d89df416f7e78a97e6f7146bd9a53238
Tikita Peralta <112982478+TikitaPeralta@users.noreply.github.com>
2023-01-29 13:06:13 +0000

    horizontal win works

---
+++ Connect_4/draftConnect4.py
@@ -0,0 +1,83 @@
+import random
+
+board = [[0,0,0,0], [0,0,0,0], [0,0,0,0], [0,0,0,0]]
+
+def print_game_board():
+    for row in board:
+        for token in row:
+            if token == 1:
+                print("X", end=" ")
+            elif token == 2:
+                print("O", end=" ")
+            else:
+                print("-", end=" ")
+        print()        
+
+
+# allows a player to place a token in the board:
+def place_a_token(token, column):
+    if board[0][column] > 0:
+        print("That column has already been filled!")
+        return False
+    
+    for row in range(len(board)):
+        if board[row][column] > 0:
+            board[row -1][column] = token
+            return True
+    board[-1][column] = token
+    return True
+
+
+# checking if a player has won:
+def check_horizontal_win(board):
+    for row in board:
+        for token in [token for token in row if token != 0]:
+            if row.count(token) > 3:    # checking for more than 3 duplicates
+                print("Player has won!")
+                return True
+    return False
+
+# if board is bigger, it will return True - need to think of a way that will recognise that the counters are next to each other, not: [0,1,1,1,2,1,0,0]
+        
+#def check_vertical_win(column):
+    # for row in board:
+    #     if board[0][column] == board[:-1][column]:
+    #         pass
+
+#def check_diagonal_left(column, board):
+    # col = range(len(column))
+    # for row in board:
+    #     if board[-1][-1] == board[:-1][col]:
+    #         return True
+    #     return False
+
+#def check_diagonal_right(column, board):
+    # col = range(len(column))    # column is undefinied, maybe row[i] = col?
+    # for row in board:
+    #     if board[-1][0] == board[-1:][col]:
+    #         return True
+    #     return False     
+
+def player_won():
+        return check_horizontal_win(board)
+        #check_vertical_win()
+        #check_diagonal_left()
+        #check_diagonal_right()``
+
+# the main game:
+if __name__ == "__main__":
+    
+    while not player_won():
+        print_game_board()
+        choose_a_column = input("what column would you like to place your token in? (type: 1/2/3/4) ")
+        p1 = int(choose_a_column) - 1
+        if not place_a_token(1, p1):
+            print()
+            continue
+        else:
+            input_token = random.randint(1, len(board[0]))
+            p2 = input_token - 1
+            while not place_a_token(2, p2):
+                input_token = random.randint(1, len(board[0]))
+                p2 = input_token - 1
+    print_game_board()              
---
+++ Connect_4/test_connect4.py
@@ -0,0 +1,25 @@
+
+from Connect_4.draftConnect4 import *
+
+def test_demo():
+    assert 3 != 2
+
+def test_horizontal_win_on_empty_board():
+    board = [[0,0,0,0], [0,0,0,0], [0,0,0,0], [0,0,0,0]]
+    assert not check_horizontal_win(board)
+
+def test_horizontal_win_on_non_win():
+    board = [[0,1,1,0], [1,0,0,1], [1,0,1,1], [0,1,0,0]]
+    assert not check_horizontal_win(board)
+
+def test_horizontal_win_on_non_win_mix():
+    board = [[0,1,1,0], [1,0,0,1], [1,2,1,1], [0,1,0,0]]
+    assert not check_horizontal_win(board)
+
+def test_horizontal_win_on_win():
+    board = [[0,0,0,0], [1,1,1,1], [0,0,0,0], [0,0,0,0]]
+    assert check_horizontal_win(board)
+
+def test_horizontal_win_on_win_p2():
+    board = [[0,0,0,0], [1,1,0,1], [2,2,2,2], [0,0,0,0]]
+    assert check_horizontal_win(board)
