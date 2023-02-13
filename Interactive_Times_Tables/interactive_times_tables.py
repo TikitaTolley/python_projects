3846797337fa35e3b5b5ac25c4dde12feadcb9c5
Tikita Peralta <112982478+TikitaPeralta@users.noreply.github.com>
2023-02-13 15:20:42 +0000

    finished times table game

--- Interactive_Times_Tables/interactive_times_tables.py
+++ Interactive_Times_Tables/interactive_times_tables.py
@@ -1,4 +1,5 @@
 import os
+from time import sleep
 import random
 
 def ClearScreen():
@@ -8,20 +9,20 @@ def ClearScreen():
 user_input = 0
 
 while True:
-    question_1 = input("Would you like to practice your times tables or dare to try the random multiplication game? (Or Q to quit) ").lower()    
-    if question_1 == "times tables":
+    question_1 = input("Would you like to practice your times tables or dare to try the random multiplication game? (tt, rmg or q to quit) ").lower()    
+    if question_1 == "tt":
         print("You chose Times Tables!")
         times_tables = input("Choose which times table you'd like to practice today? (1/2/3/4/5/6/7/8/9/10/11/12/13) ")
         while True:
             random_number = random.randint(0, 14)
             
-            answer_1 = input("(Q to quit) " + times_tables + " x " +str(random_number) + " = ").lower()
-            if answer_1 == times_tables*random_number:
+            answer_1 = input("(q to quit) " + times_tables + " x " + str(random_number) + " = ").lower()
+            if answer_1 == str(int(times_tables) * random_number):
                 user_input += 1
                 print("You got it right!")
                 continue
 
-            elif answer_1 == "Q":
+            elif answer_1 == "q":
                 break
             
             else:
@@ -30,31 +31,32 @@ while True:
 
 
         
-    elif question_1 == "random multiplication game":
+    elif question_1 == "rmg":
         print("You chose the random multiplication game!")
         while True:
             rand_numb_1 = random.randint(0, 14)
             rand_numb_2 = random.randint(0, 14)
 
-            answer_2 = input("(Q to quit) " + str(rand_numb_1) + " x " + str(rand_numb_2) + " = ").lower()
-            if answer_2 == rand_numb_1*rand_numb_2:
+            answer_2 = input("(q to quit) " + str(rand_numb_1) + " x " + str(rand_numb_2) + " = ").lower()
+            if answer_2 == str(rand_numb_1 * rand_numb_2):
                 user_input += 1
                 print("You got it right!")
                 continue
 
-            elif answer_2 == "Q":
+            elif answer_2 == "q":
                 break
             
             else:
                 print("Better luck next time!")
                 continue
     
-    elif question_1 == "Q":
+    elif question_1 == "q":
         break
 
     else:
         continue
 
-print("You got " + user_input + "questions right!")
+print("You got " + str(user_input) + " questions right!")
 print("Thanks for playing!")
+sleep(5)
 ClearScreen()
