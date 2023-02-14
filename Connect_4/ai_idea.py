391948354e8478e87e134bb7ea4f22346c9098c6
Tikita Peralta <112982478+TikitaPeralta@users.noreply.github.com>
2023-02-14 14:40:55 +0000

    added some ideas to ai but nothing works

--- Connect_4/ai_idea.py
+++ Connect_4/ai_idea.py
@@ -1,5 +1,5 @@
-board = [[0 for x in range(4)] for y in range(6)]
-
+# board = [[0 for x in range(4)] for y in range(6)]
+board = [[0,2,0,0],[2,0,0,0],[1,0,2,0],[2,1,2,0]]
 print(board)
 
 def print_board():
@@ -11,5 +11,40 @@ def print_board():
                 print('O', end=" ")
             else:
                 print("-", end=" ")
+        print()
 
 print_board()
+
+
+def checking_computer_tokens(board):
+    l = []
+    rows = list()
+    cols =
+    for row in board:
+        for token in row:
+            if row[token]==2:
+                l.append(board[row-1][token])
+                l.append(board[row-1][token+1])
+                l.append(board[row][token+1])
+                l.append(board[row+1][token+1])
+                l.append(board[row+1][token])
+                l.append(board[row][token-1])
+                l.append(board[row-1][token-1])
+                # l = list([board[row-1][token],
+                #  board[row-1][token+1], 
+                # board[row][token+1], 
+                # board[row+1][token+1], 
+                # board[row+1][token], 
+                # board[row+1][token-1], 
+                # board[row][token-1], 
+                # board[row-1][token-1]])
+            
+
+print(checking_computer_tokens(board))
+
+    
+
+                    
+
+
+
