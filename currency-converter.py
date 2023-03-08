b0b6fe30c53ec7bcb9a27a3e9f6222096fab65ac
Tikita Peralta <112982478+TikitaPeralta@users.noreply.github.com>
2023-03-08 06:47:27 +0000

    added currency

---
+++ currency-converter.py
@@ -0,0 +1,14 @@
+money = {'baht' : '41.46',
+         'dong' : '28,079.27',
+         'euro' : '1.12',
+         'rupiah' : '18,278.35',
+         }
+
+def converter():
+    amount*
+
+def converting_currency():
+    amount = input("How much would you like to convert? (write number:) ")
+    currency_from = input("What currency would you like to convert from? (write word:) ").lower()
+    currency_to = input("What currency would you like to convert to? (write word:) ").lower()
+
