fe10d6d04c04f965dc53606502932d046c2d62cb
Tikita Peralta <112982478+TikitaPeralta@users.noreply.github.com>
2023-03-08 07:45:33 +0000

    searching through dict

--- currency-converter.py
+++ currency-converter.py
@@ -1,14 +1,17 @@
-money = {'baht' : '41.46',
-         'dong' : '28,079.27',
-         'euro' : '1.12',
-         'rupiah' : '18,278.35',
-         }
+money = {'baht' : 41.46,
+         'dong' : 28079.27,
+         'euro' : 1.12,
+         'rupiah' : 18278.35,
+         'yen' : 162.78}
 
-def converter():
-    amount*
 
 def converting_currency():
-    amount = input("How much would you like to convert? (write number:) ")
-    currency_from = input("What currency would you like to convert from? (write word:) ").lower()
-    currency_to = input("What currency would you like to convert to? (write word:) ").lower()
+    amount = input("How much would you like to convert? (write number:) £")
+    currency_to = input("What currency would you like to convert to? (write word:) ").lower()       # key
+    for m in money:
+        if currency_to == m:
+            converted = amount*money[int(m)]
+    return converted
 
+
+converting_currency()
