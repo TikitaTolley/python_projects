d46b5242575e515cfebd20d62cbd903046b17a40
Tikita Peralta <tikitaperalta@gmail.com>
2023-05-06 23:58:55 +0700

    add key strokes

--- Ping_Pong/pingpong.py
+++ Ping_Pong/pingpong.py
@@ -15,6 +15,7 @@ def play():
     player1_score = 0
     player2_score = 0
     font = pygame.font.SysFont('arial', 30)
+    dist_from_top = [150,150]
 
     while run:
         delta_t = clock.tick()
@@ -28,9 +29,22 @@ def play():
         text = font.render(f'Score: {player1_score} : {player2_score}', True, white)
         screen.blit(text, (400,10,500,200))
 
-        player1 = pygame.draw.rect(screen, white, (90, 150, 10, 100))  # (left, top, width, height)
+        player1 = pygame.draw.rect(screen, white, (910, dist_from_top[0], 10, 100))  # (left, top, width, height)
+        player2 = pygame.draw.rect(screen, white, (90, dist_from_top[1], 10, 100))
 
-        player2 = pygame.draw.rect(screen, white, (910, 150, 10, 100))
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_UP]:
+            dist_from_top[0] -= 1
+        if keys[pygame.K_DOWN]:
+            dist_from_top[0] += 1
+        
+        if keys[pygame.K_w]:
+            dist_from_top[1] -= 1
+        if keys[pygame.K_s]:
+            dist_from_top[1] += 1
+
+        ball = pygame.draw.circle(screen, white, (500, 300), 20 )
+        
 
         display.blit(screen, (0,0))
         pygame.display.update()
