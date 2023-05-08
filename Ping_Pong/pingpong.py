1d98646a68cde271ddb11f3c436706eb5770f779
Tikita Peralta <tikitaperalta@gmail.com>
2023-05-08 12:25:02 +0700

    add function for middle line

--- Ping_Pong/pingpong.py
+++ Ping_Pong/pingpong.py
@@ -1,8 +1,22 @@
 import pygame
+import numpy as np
 
 W, H = 1000, 600
 black = (0, 0, 0)
 white = (255, 255, 255)
+
+def draw_dashed_line(surf, color, start_pos, end_pos, width=1, dash_length=10):
+    origin = start_pos[1]
+    target = end_pos[1]
+    length = int(target - origin)
+    dash_amount = int(length/dash_length)
+    print(f'length: {length}, dash_amount: {dash_amount}')
+
+    for index in range(0, dash_amount, 2):
+        start = origin + (dash_amount *    index    * dash_length)
+        print(start)
+        end   = origin + (dash_amount * (index + 1) * dash_length)
+        pygame.draw.line(surf, color, (500, start), (500,end), width)
 
 def play():
     pygame.init()
@@ -28,6 +42,8 @@ def play():
 
         text = font.render(f'Score: {player1_score} : {player2_score}', True, white)
         screen.blit(text, (400,10,500,200))
+
+        draw_dashed_line(screen, white, (500, 0), (500, 600))
 
         player1 = pygame.draw.rect(screen, white, (910, dist_from_top[0], 10, 100))  # (left, top, width, height)
         player2 = pygame.draw.rect(screen, white, (90, dist_from_top[1], 10, 100))
