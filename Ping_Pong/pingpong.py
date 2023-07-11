0e93db8bab4432f45266d177a1ec2ca1ec055124
Tikita Peralta <tikitaperalta@gmail.com>
2023-07-11 19:33:56 +0900

    finished pingpong game

--- Ping_Pong/pingpong.py
+++ Ping_Pong/pingpong.py
@@ -32,10 +32,11 @@ def play():
     run = True
     player1_score = 0
     player2_score = 0
-    font = pygame.font.SysFont('arial', 30)
+    font = pygame.font.SysFont('monospace', 30)
     dist_from_top = [150,150]
     center_ball = [W/2, H/2]
-    velocity = 10
+    velocityX = 2
+    velocityY = 2
 
     counter, text = 10, '10'.rjust(3)
     pygame.time.set_timer(pygame.USEREVENT, 1000)
@@ -59,80 +60,91 @@ def play():
 
         draw_dashed_line(screen, white, (500, 0), (500, 600))
 
+        milli_since_last_frame = clock.tick(30)    # rate of each frame
+        ticks = pygame.time.get_ticks()     # gives time program's been running in milliseconds
+        seconds = ticks/1000
+        print(seconds)                
+        posX, posY = center_ball
+
+        # line connections (ball + paddle):
+        player2_dist_to_ball = line_length([posX, posY], [90, dist_from_top[1] + 50])
+        #pygame.draw.line(screen, grey, [posX, posY], [90, dist_from_top[1] + 50])
+
+        player1_dist_to_ball = line_length([posX, posY], [920, dist_from_top[0] + 50])
+        #pygame.draw.line(screen, grey, [posX, posY], [920, dist_from_top[0] + 50])
+
+        # basic movement and bouncing:
+        if posY >= 600 or posY < 0:
+            velocityY *= -1
+        posY += velocityY
+        posX += velocityY
+
+        # paddle-ball bouncing p1:
+        if player1_dist_to_ball <= 20:
+            velocityX *= -1
+            velocityY *= -1
+        posX += velocityX
+        posY += velocityY
+
+        # paddle-ball bouncing p2:
+        if player2_dist_to_ball <= 20:
+            velocityX *= -1
+            velocityY *= -1
+        posX += velocityX
+        posY += velocityY
+
+        # scoring:
+        if posX >= 1000:
+            player2_score += 1
+            posX = W/2 
+            posY = H/2
+        posX += velocityX
+        posY += velocityY
+
+        if posX < 0:
+            player1_score += 1
+            posX = W/2
+            posY = H/2
+        posX += velocityX
+        posY += velocityY
+
+        center_ball = posX, posY
+        #print(posX, posY, velocityX, velocityY)
+        
+
+        #horizontal = pygame.draw.line(screen, brown, [posX, posY], [posX + 200, posY])
+        #vertical = pygame.draw.line(screen, brown, [posX, posY], [posX, posY + 200])
+
         player1 = pygame.draw.rect(screen, white, (910, dist_from_top[0], 10, 100))  # (left, top, width, height)
         player2 = pygame.draw.rect(screen, white, (90, dist_from_top[1], 10, 100))
-        ball = pygame.draw.circle(screen, white, center_ball, 20)
+        ball = pygame.draw.circle(screen, white, [posX, posY], 20)
 
         keys = pygame.key.get_pressed()
-        if keys[pygame.K_UP]:
-            dist_from_top[0] -= 1
-        if keys[pygame.K_DOWN]:
-            dist_from_top[0] += 1
+        if keys[pygame.K_UP] and dist_from_top[0] > 0:
+            dist_from_top[0] -= 10
+        if keys[pygame.K_DOWN] and dist_from_top[0] < 500:
+            dist_from_top[0] += 10
         
-        if keys[pygame.K_w]:
-            dist_from_top[1] -= 1
-        if keys[pygame.K_s]:
-            dist_from_top[1] += 1
+        if keys[pygame.K_w] and dist_from_top[1] > 0:
+            dist_from_top[1] -= 10
+        if keys[pygame.K_s] and dist_from_top[1] < 500:
+            dist_from_top[1] += 10
 
-        #ball_radius = line_length(center_ball, [center_ball[0] + 20, center_ball[1]])
-        width_to_diag = range(20, int(((2600)**(1/2))+10))
-
-        player2_dist_to_ball = line_length(center_ball, [90, dist_from_top[1] + 50])
-        pygame.draw.line(screen, grey, center_ball, [90, dist_from_top[1] + 50])
-
-        player1_dist_to_ball = line_length(center_ball, [920, dist_from_top[0] + 50])
-        pygame.draw.line(screen, grey, center_ball, [920, dist_from_top[0] + 50])
         
-        # ball movement and collisons:
-
-        # calculate balls angle to the horizontal: (draw both lines, and angle)
-
-        # motion as velocity in angle's direction
-
-        # start of game: random ball angle
-
-        # bounce, opposite direction
-
-        #reset position torwards loser
-
-        '''
-        i = 10
-        while i > 0:
-            i - 1
-            print(i)
-            if center_ball[1] >= 600 or center_ball[1] <= 0:    # hitting sides, bottom - top
-                #center_ball[0] += 1
-                center_ball[1] -= 1
-
-            if player1_dist_to_ball in width_to_diag:
-                print('player 1 hits!')
-            if player2_dist_to_ball in width_to_diag:
-                print('player 2 hits!')            
-            
-            # scoring:
-            if center_ball[0] <= 0:   # hitting sides, left
-                player1_score +=1
-                center_ball = [W/2, H/2]    # reset
-            if center_ball[0] >= 1000:  # hitting sides, right
-                player2_score += 1
-                center_ball = [W/2, H/2]    # reset
-            
-            center_ball[0] -= 1
-            #center_ball[1] += 1
-
-
-        if i == 0:
+        
+        # end screen:
+        if seconds >= 10:
             screen.fill(black)
             if player1_score > player2_score:
                 winning1 = font.render('player 1 wins!', True, white)
                 screen.blit(winning1, (500,300,500,200))
-            if player2_score > player1_score:
+            elif player2_score > player1_score:
                 winning2 = font.render('player 2 wins!', True, white)
                 screen.blit(winning2, (500,300,500,200))
-            if player1_score == player2_score:
+            else:
                 winning3 = font.render('its a draw!', True, white)
                 screen.blit(winning3, (500,300,500,200))
-'''
+            
         
         display.blit(screen, (0,0))
         pygame.display.update()
